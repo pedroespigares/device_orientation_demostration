12f276a96b9453170d9645a7188926f567a5bddc
Pedro Espigares Asenjo <pedritoesp04@gmail.com>
2022-10-30 19:29:32 +0100

    Invert gamma y beta

--- device_orientation.js
+++ device_orientation.js
@@ -14,11 +14,11 @@ function handleOrientation(event) {
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
-  if (x > 90) {
-    x = 90;
+  if (y > 90) {
+    y = 90;
   }
-  if (x < -90) {
-    x = -90;
+  if (y < -90) {
+    y = -90;
   }
 
   // To make computation easier we shift the range of
